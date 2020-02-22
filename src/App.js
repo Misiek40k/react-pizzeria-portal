f97a8f3ea3c45ad3552d53cf78338db059507814
Misiek40k <comanderrr@gmail.com>
2020-02-22 20:03:31 +0100

    Add subviews routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Login from './components/views/Login/Login';
 import Tables from './components/views/Tables/Tables';
 import Waiter from './components/views/Waiter/Waiter';
 import Kitchen from './components/views/Kitchen/Kitchen';
+import OrderNew from './components/views/Waiter/OrderNew';
+import OrderItem from './components/views/Waiter/OrderItem';
+import BookingNew from './components/views/Tables/BookingNew';
+import BookingItem from './components/views/Tables/BookingItem';
+import EventsNew from './components/views/Tables/EventsNew';
+import EventsItem from './components/views/Tables/EventsItem';
 
 function App() {
   return (
@@ -18,6 +24,12 @@ function App() {
           <Route exact path={`${process.env.PUBLIC_URL}/tables`} component={Tables} />
           <Route exact path={`${process.env.PUBLIC_URL}/waiter`} component={Waiter} />
           <Route exact path={`${process.env.PUBLIC_URL}/kitchen`} component={Kitchen} />
+          <Route exact path={`${process.env.PUBLIC_URL}/waiter/order/new`} component={OrderNew} />
+          <Route exact path={`${process.env.PUBLIC_URL}/waiter/order/:id`} component={OrderItem} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/new`} component={BookingNew} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables/booking/:id`} component={BookingItem} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables/events/new`} component={EventsNew} />
+          <Route exact path={`${process.env.PUBLIC_URL}/tables/events/:id`} component={EventsItem} />
         </Switch>
       </MainLayout>
     </BrowserRouter>
